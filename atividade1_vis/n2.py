aa4e9edfd4bc2b09c2f227b8b71e6d3083a74796
Certezas <93018888+Certezas@users.noreply.github.com>
2021-11-26 17:58:50 -0300

    Delete atividade1_vis directory

--- atividade1_vis/n2.py
+++
@@ -1,9 +0,0 @@
-num1 = input('Digite o primeiro número\n')
-num2 = input('Digite o segundo número\n')
-num3 = input('Digite o terceiro número\n')
-
-numeros = (num1, num2, num3)
-lista = list(numeros)
-lista.sort(reverse = True)
-
-print(lista)
